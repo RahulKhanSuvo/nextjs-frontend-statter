4694bceb270f9acb42dca45c059e2d937b5d9188
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-23 00:19:43 +0600

    feat: implement login page UI

--- src/app/(commonLayout)/(auth)/login/page.tsx
+++ src/app/(commonLayout)/(auth)/login/page.tsx
@@ -1,4 +1,10 @@
+import LoginForm from '@/features/auth/components/LoginForm';
+
 const LoginPage = () => {
-  return <>LoginPage</>;
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
 };
 export default LoginPage;
