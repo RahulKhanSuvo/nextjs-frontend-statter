ea88cb867a0cae90d0c59337df38de7b5f3e4398
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-24 00:08:13 +0600

    feat: add DashboardNavbar to dashboard layout

--- src/app/(dashbaordlayout)/layout.tsx
+++ src/app/(dashbaordlayout)/layout.tsx
@@ -1,4 +1,5 @@
 import { getCurrentUser } from '@/features/auth/action';
+import DashboardNavbar from '@/features/dashboard/components/DashboardNavbar';
 import DashboardSideBarContent from '@/features/dashboard/components/DashboardSideBarContent';
 import { getNavItemsByRole } from '@/features/dashboard/data/navItem';
 
@@ -15,7 +16,8 @@ export default async function DashboardLayout({
   return (
     <div className="flex h-screen">
       <DashboardSideBarContent userInfo={userInfo} navItems={navItems} />
-      <div>
+      <div className="flex flex-1 flex-col overflow-hidden">
+        <DashboardNavbar userInfo={userInfo} navItems={navItems} />
         <main>{children}</main>
       </div>
     </div>
