3c514daf4336b2c82cf23ec576d5573ce1185916
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-23 21:41:00 +0600

    feat: implement dashboard layout with sidebar navigation

--- src/app/(dashbaordlayout)/layout.tsx
+++ src/app/(dashbaordlayout)/layout.tsx
@@ -1,7 +1,23 @@
-export default function DashboardLayout({
+import { getCurrentUser } from '@/features/auth/action';
+import DashboardSideBarContent from '@/features/dashboard/components/DashboardSideBarContent';
+import { getNavItemsByRole } from '@/features/dashboard/data/navItem';
+
+export default async function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  const userInfo = await getCurrentUser();
+  if (!userInfo) {
+    return null;
+  }
+  const navItems = getNavItemsByRole(userInfo.role);
+  return (
+    <div className="flex h-screen">
+      <DashboardSideBarContent userInfo={userInfo} navItems={navItems} />
+      <div>
+        <main>{children}</main>
+      </div>
+    </div>
+  );
 }
