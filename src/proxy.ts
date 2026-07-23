1ee895542367c75d8278a50954555111dc920477
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-23 19:38:41 +0600

    feat: implement role-based route access control in proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,17 +1,28 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { getDefaultRoute, isAuthRoute, UserRole } from './lib/authUtil';
+import { getDefaultRoute, getRouteOwner, isAuthRoute, UserRole } from './lib/authUtil';
 import { getCurrentUser } from './features/auth/action';
 
 export async function proxy(request: NextRequest) {
   try {
     const { pathname } = request.nextUrl;
     const user = await getCurrentUser();
-    console.log('user from proxy', user);
     const isAuth = isAuthRoute(pathname);
+    const routeOwner = getRouteOwner(pathname);
+    console.log('user from proxy', user);
+    console.log('routeOwner', routeOwner);
+    console.log('isAuthRoute', isAuth);
+
     if (isAuth) {
       return NextResponse.redirect(new URL(getDefaultRoute(user?.role as UserRole), request.url));
     }
-    console.log('isAuthRoute', isAuth);
+    if (routeOwner === 'COMMON') {
+      return NextResponse.next();
+    }
+    if (routeOwner === 'DOCTOR' || routeOwner === 'PATIENT' || routeOwner === 'SUPER_ADMIN') {
+      if (routeOwner !== (user?.role as UserRole)) {
+        return NextResponse.redirect(new URL(getDefaultRoute(user?.role as UserRole), request.url));
+      }
+    }
   } catch (error) {
     console.error('Proxy Error:', error);
     return NextResponse.next();
